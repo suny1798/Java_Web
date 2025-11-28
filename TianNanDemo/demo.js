f86e897f2858dbe1772890fd516e7ab3c82c7dca
两星期没洗头好痒 <suny1798@qq.com>
2025-11-28 20:48:38 +0800

    function

--- TianNanDemo/demo.js
+++ TianNanDemo/demo.js
@@ -1,1 +1,19 @@
-alert("欢迎来到天南大陆智能学习辅助系统！")
+// alert("欢迎来到天南大陆智能学习辅助系统！")
+
+
+
+function show_alert(){
+    alert("欢迎来到天南大陆智能学习辅助系统！")
+}
+
+
+
+show_alert();
+
+
+function add(a, b){
+    return a + b;
+}
+
+
+console.log(add(5, 10));
